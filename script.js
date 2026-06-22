362f4d5bf2497194aff34d1947dc5f3916cf1a2f
lafaye-coding <lafayebre@gmail.com>
2026-06-22 09:59:31 -0700

    initialized repository and added my first code

---
+++ script.js
@@ -0,0 +1,25 @@
+function happy() {
+   document.getElementById("message").textContent = "Happy mode activated";
+   document.getElementById("page").style.background = "yellow";
+}
+
+
+function sad(){
+    document.getElementById("message").textContent = "sad mode activated";
+    document.getElementById("page").style.background = "#1c6679";
+}
+
+
+function jealous() {
+    document.getElementById("message").textContent = "Jealous mode activated"; document.getElementById("page").style.background = "pink"
+}
+
+
+function angry() {
+    document.getElementById("message").textContent = "Angry mode activiated"; document.getElementById("page").style.background = "red"
+}
+
+
+function anxious(){
+    document.getElementById("message").textContent = "Anxious mode activated"; document.getElementById("page").style.background = "grey"
+}
